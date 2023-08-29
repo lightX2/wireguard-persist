244ca581f0da5f73327734753724f0ec48ea739d
lightX2 <lightsx2@gmail.com>
2023-08-29 17:29:29 +0500

    fix check

--- wg-persistent/usr/bin/wg-persistent.sh
+++ wg-persistent/usr/bin/wg-persistent.sh
@@ -9,30 +9,28 @@ CHECK_FILE=/tmp/wg-persist-$2
 i=1
 failcount=0
 let "failcount += $(cat CHECK_FILE)"
-
 while [ $i -le 7 ]
 do
   let "i += 1"
   if ping -c 1 -W 3 $1 > /dev/null
   then
     echo 0 > CHECK_FILE
+   
     exit 0
   fi
-    
+
   echo no connection, mark it
   let "failcount=failcount+1"
   
   echo $failcount > CHECK_FILE
-  cat CHECK_FILE
   
-  if [ $failcount > 5 ] ; then
+  if [ $failcount -ge 5 ] ; then
     systemctl stop wg-quick@$2
     sleep 2
     systemctl start wg-quick@$2
     rm CHECK_FILE
     exit 0
   fi
-  
   sleep 4
 
 done
